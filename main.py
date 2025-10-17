7f1ba0adb563492b43e0ce24b0f3ee6cc15decd1
George v s <george@superion.in>
2025-10-17 15:24:49 +0530

    Fix:/api/utility-service/

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from tools import pdf_endpoint
 from tools import presentation_endpoint 
 from tools import doc_endpoint
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 #Adding the endpoints to the main app
-
+'''
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # For development only. Specify domains in production.
@@ -13,11 +13,11 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allows all headers
 )
+'''
 
-
-app.include_router(pdf_endpoint.router, prefix="/api/pdf", tags=["PDF"])
-app.include_router(presentation_endpoint.pptx_router, prefix="/api/pptx", tags=["PPTX"])
-app.include_router(doc_endpoint.docx_router, prefix="/api/docx", tags=["DOCX"])
+app.include_router(pdf_endpoint.router, prefix="/api/utility-service/pdf/generate", tags=["PDF"])
+app.include_router(presentation_endpoint.pptx_router, prefix="/api/utility-service/pptx/generate", tags=["PPTX"])
+app.include_router(doc_endpoint.docx_router, prefix="/api/utility-service/docx/generate", tags=["DOCX"])
 
 
 @app.get("/")
